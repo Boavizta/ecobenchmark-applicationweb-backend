124e983574ec97b40c2744a5b6f0918728f68d69
Jérémie Drouet <jeremie.drouet@datadoghq.com>
2022-04-29 14:08:04 +0000

    feat(rust-actix-sqlx): add create account endpoint
    Signed-off-by: Jérémie Drouet <jeremie.drouet@datadoghq.com>

---
+++ service/rust-actix-sqlx/src/error.rs
@@ -0,0 +1,41 @@
+use actix_web::http::StatusCode;
+use actix_web::{HttpResponse, HttpResponseBuilder, ResponseError};
+
+#[derive(Debug)]
+pub struct Error {
+    code: StatusCode,
+    message: String,
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Error {}: {:?}", self.code.as_u16(), self.message)
+    }
+}
+
+impl ResponseError for Error {
+    fn status_code(&self) -> StatusCode {
+        self.code
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        HttpResponseBuilder::new(self.code).json(&serde_json::json!({
+            "message": self.message
+        }))
+    }
+}
+
+impl From<sqlx::Error> for Error {
+    fn from(err: sqlx::Error) -> Self {
+        match err {
+            sqlx::Error::RowNotFound => Self {
+                code: StatusCode::NOT_FOUND,
+                message: "Entity not found.".into(),
+            },
+            _ => Self {
+                code: StatusCode::INTERNAL_SERVER_ERROR,
+                message: "Internal server error.".into(),
+            },
+        }
+    }
+}
---
+++ service/rust-actix-sqlx/src/handler/mod.rs
@@ -0,0 +1,2 @@
+pub(crate) mod accounts_create;
+pub(crate) mod health;
