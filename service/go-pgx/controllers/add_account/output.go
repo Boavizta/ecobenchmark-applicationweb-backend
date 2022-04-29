c66fbd77a50fe0ad3fb2843f0f32a1d0425eca77
Youen Chéné <youen.chene@ekwateur.fr>
2022-04-29 09:04:24 +0200

    fix: following merge request comments

--- service/go-pgx/controllers/add_account/output.go
+++ service/go-pgx/controllers/add_account/output.go
@@ -2,6 +2,6 @@ package add_account
 
 type responseBody struct {
 	Id           string `json:"id,omitempty"`
-	Login        string `json:"name,omitempty"`
+	Login        string `json:"login,omitempty"`
 	CreationDate string `json:"creation_date"`
 }
