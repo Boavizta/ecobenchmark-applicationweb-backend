376754779b8eeacc1cdfebda5d79e9bdd03f13dc
Jonathan Germond <jonathan+git@germond.email>
2022-06-02 14:04:10 +0200

    feat(php-symfony) Use lazy loading for GetLists endpoint

--- service/php-symfony/src/Controller/GetLists/GetLists.php
+++ service/php-symfony/src/Controller/GetLists/GetLists.php
@@ -9,14 +9,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Uid\UuidV4;
 
 class GetLists extends AbstractController
 {
     #[Route('/api/accounts/{account_id}/lists', name: 'getLists', methods: 'GET')]
     public function getLists(Request $request, string $account_id, ManagerRegistry $doctrine): Response
     {
+        $entityManager = $doctrine->getManager();
         $listRepository = $doctrine->getManager()->getRepository(ListEntity::class);
-        $lists = $listRepository->findAllByAccountJoinTasks($account_id, $request->query->getInt('page'));
+        $lists = $listRepository->findBy(
+            array('account' => $entityManager->getReference('App\Entity\Account', new UuidV4($account_id))),
+            array(),
+            10,
+            $request->query->getInt('page') * 10
+        );
 
         return $this->json(ListResponse::fromListEntities($lists));
     }
--- service/php-symfony/src/Repository/ListEntityRepository.php
+++ service/php-symfony/src/Repository/ListEntityRepository.php
@@ -47,48 +47,4 @@ class ListEntityRepository extends ServiceEntityRepository
             $this->_em->flush();
         }
     }
-
-    /**
-     * @param string $account_id
-     * @param int $page
-     * @param int $pageSize
-     * @return ListEntity[]
-     */
-    public function findAllByAccountJoinTasks(string $account_id, int $page, int $pageSize = 10): array
-    {
-        $entityManager = $this->getEntityManager();
-
-        $rsm = new ResultSetMappingBuilder($entityManager);
-        $rsm->addRootEntityFromClassMetadata('App\Entity\ListEntity', 'l');
-        $rsm->addJoinedEntityFromClassMetadata(
-            'App\Entity\Task',
-            't',
-            'l',
-            'tasks',
-            ['id' => 'task_id', 'name' => 'task_name', 'creation_date' => 'task_creation_date']
-        );
-
-        $query = $entityManager->createNativeQuery(
-            'SELECT
-                l.id,
-                l.name,
-                l.creation_date,
-                l.account_id,
-                t.id AS task_id,
-                t.name AS task_name,
-                t.description,
-                t.creation_date AS task_creation_date
-            FROM list l
-                LEFT JOIN task t ON l.id = t.list_id
-            WHERE
-                l.account_id = :id
-                AND l.id IN (SELECT id FROM list WHERE account_id = :id LIMIT :limit OFFSET :offset)',
-            $rsm
-        )
-            ->setParameter('id', $account_id)
-            ->setParameter('limit', $pageSize)
-            ->setParameter('offset', $page * $pageSize);
-
-        return $query->getResult();
-    }
 }
