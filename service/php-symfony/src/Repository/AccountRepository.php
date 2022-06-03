856ec95fbfa211aeb365f080622af59a70384e1e
Jonathan Germond <jonathan+git@germond.email>
2022-06-03 15:27:54 +0200

    feat(php-symfony) aggregation on code side

--- service/php-symfony/src/Repository/AccountRepository.php
+++ service/php-symfony/src/Repository/AccountRepository.php
@@ -54,33 +54,54 @@ class AccountRepository extends ServiceEntityRepository
         $entityManager = $this->getEntityManager();
 
         $rsm = new ResultSetMapping();
-        $rsm->addScalarResult('id', 'id');
+        $rsm->addScalarResult('account_id', 'account_id');
         $rsm->addScalarResult('login', 'login');
-        $rsm->addScalarResult('nb_list', 'nb_list');
-        $rsm->addScalarResult('avg_tasks', 'avg_tasks', 'float');
+        $rsm->addScalarResult('list_id', 'list_id');
+        $rsm->addScalarResult('task_id', 'task_id');
 
 
         $query = $entityManager->createNativeQuery(
-            'SELECT 
-				id,  
-				login, 
-				count(list_id) AS nb_list, 
-				round(avg(nb_tasks),2) AS avg_tasks 
-			FROM (
-				SELECT 
-					account.id, 
-					account.login, 
-					list.id list_id, 
-					count(task.id) nb_tasks 
-				FROM account
-				INNER JOIN list ON (list.account_id=account.id) 
-				LEFT JOIN task ON (task.list_id=list.id) 
-				GROUP BY account.id, account.login, list.id
-			) t 
-			GROUP BY id, login',
+            'SELECT account.id account_id, account.login, list.id list_id, task.id task_id
+            FROM account
+                INNER JOIN list ON (list.account_id=account.id)
+                LEFT JOIN task ON (task.list_id=list.id)',
             $rsm
         );
 
-        return $query->getArrayResult();
+        $rawStats = $query->getArrayResult();
+        $preparedData = [];
+        foreach ($rawStats as $rawLine) {
+            $account_id = $rawLine['account_id'];
+            $list_id = $rawLine['list_id'];
+            if (array_key_exists($account_id, $preparedData)) {
+                if (!array_key_exists($list_id, $preparedData[$account_id]['task_id_map'])) {
+                    $preparedData[$account_id]['task_id_map'][$list_id] = 0;
+                }
+            } else {
+                $preparedData[$account_id] = [
+                    'account_id' => $account_id,
+                    'login' => $rawLine['login'],
+                    'task_id_map' => [$list_id => 0],
+                    'task_count' => 0
+                ];
+            }
+            if (!empty($rawLine['task_id'])) {
+                $preparedData[$account_id]['task_id_map'][$list_id]++;
+                $preparedData[$account_id]['task_count']++;
+            }
+        }
+
+        $stats = [];
+        foreach ($preparedData as $data) {
+            $avgTask = $data['task_count'] / count($data['task_id_map']);
+            $stats[] = [
+                'id' => $data['account_id'],
+                'login' => $data['login'],
+                'nb_list' => count($data['task_id_map']),
+                'avg_tasks' => round($avgTask, 2),
+            ];
+        }
+
+        return $stats;
     }
 }
