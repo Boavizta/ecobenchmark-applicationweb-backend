99de827d5f81f7ed4db3eb53b494806b5ea7f9c5
Mael-Brs <mael.b_53@hotmail.fr>
2022-11-07 23:42:07 +0100

    Revue API GetListsResource

--- service/jvm-quarkus/src/main/java/com/ecobenchmark/controllers/getlists/GetListsResource.java
+++ service/jvm-quarkus/src/main/java/com/ecobenchmark/controllers/getlists/GetListsResource.java
@@ -4,12 +4,9 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
-import javax.persistence.Tuple;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
-import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,11 +28,11 @@ public class GetListsResource {
 
         Query nativeQuery = entityManager.createNativeQuery("""
                 SELECT
-                    CAST(l.id AS VARCHAR),
+                    l.id,
                     l.name,
                     l.creation_date,
-                    CAST(l.account_id AS VARCHAR),
-                    CAST(t.id AS VARCHAR) AS task_id,
+                    l.account_id,
+                    t.id AS task_id,
                     t.name AS task_name,
                     t.description AS task_description,
                     t.creation_date AS task_creation_date
@@ -43,8 +40,8 @@ public class GetListsResource {
                     LEFT JOIN task t ON l.id = t.list_id
                 WHERE
                     l.account_id = :accountId
-                    AND l.id IN (SELECT id FROM list WHERE account_id = :accountId LIMIT :nbElement OFFSET :offset)""".trim(), Tuple.class);
-        List<Tuple> resultList = nativeQuery
+                    AND l.id IN (SELECT id FROM list WHERE account_id = :accountId LIMIT :nbElement OFFSET :offset)""".trim(), "getListsResponseMapping");
+        List<Object[]> resultList = nativeQuery
                 .setParameter("accountId", accountId)
                 .setParameter("nbElement", 10)
                 .setParameter("offset", page * 10)
@@ -52,31 +49,21 @@ public class GetListsResource {
 
         Map<UUID, ListResponse> listResponseMap = new LinkedHashMap<>();
 
-        for(Tuple tuple : resultList){
-            UUID listId = UUID.fromString(tuple.get("id", String.class));
-            ListResponse list = listResponseMap.computeIfAbsent(
-                    listId,
-                    id -> contructListResponse(tuple)
-            );
+        for(Object[] objects : resultList){
+            ListResponse listResponse = (ListResponse) objects[0];
 
-            list.getTasks().add(
-                    contructTaskResponse(tuple)
-            );
+            ListResponse mappedList = listResponseMap.putIfAbsent(listResponse.getId(), listResponse);
+
+            if(mappedList != null){
+                TaskResponse taskResponse = (TaskResponse) objects[1];
+                List<TaskResponse> tasks = mappedList.getTasks();
+                tasks.add(taskResponse);
+            }
 
         }
 
         return Response.ok(listResponseMap.values()).build();
     }
 
-    private ListResponse contructListResponse(Tuple tuple) {
-        Timestamp creationDate = tuple.get("creation_date", Timestamp.class);
-        Instant creationDateInstant = creationDate != null ? creationDate.toInstant(): null;
-        return new ListResponse(UUID.fromString(tuple.get("id", String.class)), tuple.get("name", String.class), creationDateInstant, UUID.fromString(tuple.get("account_id", String.class)));
-    }
 
-    private TaskResponse contructTaskResponse(Tuple tuple) {
-        Timestamp taskCreationDate = tuple.get("task_creation_date", Timestamp.class);
-        Instant creationDate = taskCreationDate.toInstant() != null ? taskCreationDate.toInstant() : null;
-        return new TaskResponse(UUID.fromString(tuple.get("task_id", String.class)), tuple.get("task_name", String.class), tuple.get("task_description", String.class), creationDate);
-    }
 }
