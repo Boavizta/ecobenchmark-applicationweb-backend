8df09ab96c5f268666a979e51d3241d062acba56
PGMU01201 <ext.guillaume.mathieu@sncf.fr>
2024-06-07 11:27:07 +0200

    Fix content-type headers

--- service/ruby-on-rails/config/application.rb
+++ service/ruby-on-rails/config/application.rb
@@ -27,5 +27,7 @@ module Ecobenchmark
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use Rack::ContentLength
   end
 end
